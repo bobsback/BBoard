45ef08538512452581be888347b7961f069e1356
Ilya Nee <neeilya@mail.ru>
2016-03-19 15:57:15 +0600

    Registered AuthPin middleware as 'auth.pin'

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,5 +32,6 @@ class Kernel extends HttpKernel
 
         'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+        'auth.pin' => \App\Http\Middleware\AuthPin::class,
     ];
 }
