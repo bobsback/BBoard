aee58799c77995825bf7b60de2f05a9644b875f4
Ilya Nee <neeilya@mail.ru>
2016-03-21 13:04:20 +0600

    Generating random access token for invites on sn saving

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Invite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Invite::saving(function($invite)
+        {
+            $invite->access_key = str_random(60);
+        });
     }
 
     /**
