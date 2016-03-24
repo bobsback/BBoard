3184fc0611b160d4132660cd5c1429b01f831bda
Ilya Nee <neeilya@mail.ru>
2016-03-24 14:00:45 +0600

    Throw proper status code on invalit JWT token

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
+                return response()->json(['error' => 'invalid_credentials'], 422);
             }
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
