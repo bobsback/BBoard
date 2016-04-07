cd86ff7a0c00066b8bc624e4f2ddf22e30bb2b5b
bobsback <bobsback99@hotmail.com>
2016-04-07 11:52:08 +0100

    togit

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,6 +1,40 @@
-<h2>Hey!</h2>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+<style>
+    html, body {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        color: #B0BEC5;
+        display: table;
+        font-weight: 100;
+        font-family: 'Roboto';
+    }
+
+    .container {
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .content {
+        text-align: center;
+        display: inline-block;
+    }
+
+    .title {
+        font-size: 20px;
+        margin-bottom: 40px;
+    }
+</style>
+<div class="container">
+<h2>Hey There!</h2>
 <p>Bubble Board is revolutionary system for feedback and discussion.</p>
 <p>You've been invited to contribute on {{ array_get($data, 'board.boardname') }} board on Bubble Board(Exciting)! </p>
 <p>
     You can enter your board here <a href="{{ url('/board/' . array_get($data, 'board.boardname') . '?access_key=' . array_get($data, 'access_key')) }}">link</a>, you must log in to and save the board to enter it again!
 </p>
+</div>
--- resources/views/emails/refer.blade.php
+++ resources/views/emails/refer.blade.php
@@ -1,3 +1,36 @@
+<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+<style>
+    html, body {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        color: #B0BEC5;
+        display: table;
+        font-weight: 100;
+        font-family: 'Roboto';
+    }
+
+    .container {
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .content {
+        text-align: center;
+        display: inline-block;
+    }
+
+    .title {
+        font-size: 20px;
+        margin-bottom: 40px;
+    }
+</style>
+<div class="container">
 <p>
     So Hey Mr {{ $email }} someone thought you might be interested in checking out our little invention for getting awesome feedback. <br><br>
 
@@ -11,4 +44,5 @@
 
 
 </p>
+    </div>
 
